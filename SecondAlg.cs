4c10eea7f4bbcf02580294d7b4c399c6d215798f
CollisionDotNet <collisiondotnet@mail.ru>
2022-10-29 19:52:10 +0300

    Create SecondAlg.cs

---
+++ SecondAlg.cs
@@ -0,0 +1,20 @@
+﻿void shellSort(int[] arr)
+{
+    int j;
+    int step = arr.Length / 2;
+    while (step > 0)
+    {
+        for (int i = 0; i < (arr.Length - step); i++)
+        {
+            j = i;
+            while ((j >= 0) && (arr[j] > arr[j + step]))
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j + step];
+                arr[j + step] = tmp;
+                j -= step;
+            }
+        }
+        step = step / 2;
+    }
+}
